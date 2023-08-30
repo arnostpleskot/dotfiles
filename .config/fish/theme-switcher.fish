2ef57807ffb3dc6e4ff8aed60e508d57e57ad799
Arnošt Pleskot <arnostpleskot@gmail.com>
2023-08-30 09:14:05 +0200

    feat: switch theme for flatpak apps

--- .config/fish/theme-switcher.fish
+++ .config/fish/theme-switcher.fish
@@ -1,24 +1,28 @@
 #!/usr/bin/env fish
 
-set LIGHTTHEME "Catppuccin Latte"
-set DARKTHEME "Catppuccin Frappe"
-set TMUX_DARK "@catppuccin_flavour 'frappe'"
-set TMUX_LIGHT "@catppuccin_flavour 'latte'"
+set THEME_LIGHT "Catppuccin Latte"
+set THEME_DARK "Catppuccin Frappe"
+set THEME_TMUX_DARK "@catppuccin_flavour 'frappe'"
+set THEME_TMUX_LIGHT "@catppuccin_flavour 'latte'"
+set THEME_FLATPAK_LIGHT Catppuccin-Latte-Standard-Lavender-Dark
+set THEME_FLATPAK_DARK Catppuccin-Mocha-Standard-Lavender-Dark
 set TMUXCONF $HOME/.config/tmux/tmux.conf
 
 echo Setting theme to $argv[1]
 
 switch $argv[1]
     case light
-        yes | fish_config theme save $LIGHTTHEME
-        sed -i "s/$TMUX_DARK/$TMUX_LIGHT/" $TMUXCONF
-        notify-send "Theme changed to light"
+        yes | fish_config theme save $THEME_LIGHT
+        sed -i "s/$THEME_TMUX_DARK/$THEME_TMUX_LIGHT/" $TMUXCONF
+        flatpak override --user --env=GTK_THEME=$THEME_FLATPAK_LIGHT
+        notify-send "Light theme" "Don't forget to restart flatpak apps"
     case dark
         echo a
-        yes | fish_config theme save $DARKTHEME
+        yes | fish_config theme save $THEME_DARK
         echo b
-        sed -i "s/$TMUX_LIGHT/$TMUX_DARK/" $TMUXCONF
-        notify-send "Theme changed to dark"
+        sed -i "s/$THEME_TMUX_LIGHT/$THEME_TMUX_DARK/" $TMUXCONF
+        flatpak override --user --env=GTK_THEME=$THEME_FLATPAK_DARK
+        notify-send "Dark theme" "Don't forget to restart flatpak apps"
     case "*"
         echo "Invalid theme option. Usage: theme-switcher.fish [light|dark]"
 end
