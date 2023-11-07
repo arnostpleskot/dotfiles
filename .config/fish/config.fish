7edbc114e10a635a628512053b372db2ac3f26e1
Arnošt Pleskot <arnostpleskot@gmail.com>
2023-11-07 15:25:35 +0100

    feat(fish): pnpm cache

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -93,3 +93,10 @@ set -U fish_greeting
 
 # Starship prompt
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/arnost/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
